7b0f67a7d15f1cae5f742df584b182c69f00b23b
Parashar Das <porashorbabu@gmail.com>
2024-08-19 22:02:20 +0600

    done just signIn logIn signOut

--- src/Components/Page/Home.jsx
+++ src/Components/Page/Home.jsx
@@ -7,13 +7,14 @@ import { getDoc,doc } from 'firebase/firestore'
 
 const Home = () => {
   const {LogOutAll} = useAuthPro()
-  const [HomeUser, setHomeUser] = useState({})
+  const [HomeUser, setHomeUser] = useState("")
   async function DataGet() {
     AppData.onAuthStateChanged(async(user)=>{
       const docRef = doc(DataStore, user?.displayName, user?.uid)
       const docSnep = getDoc(docRef)
       if(docSnep){
         console.log((await docSnep).data())
+        setHomeUser((await docSnep).data())
         toast.success("Get data is success",{
           position: "top-left"
         })
@@ -29,9 +30,10 @@ const Home = () => {
   },[])
   return (
     <div>
-      <Container>
+      {HomeUser ? <Container>
         {/* header */}
         <h1 className='text-2xl text-center font-bold'>Welcome Parashar Das</h1>
+        {/* details section */}
         <div className='grid grid-cols-2 gap-5 items-center py-5 text-xl'>
           <div className='font-semibold space-y-2'>
             <h1>First name:</h1>
@@ -39,14 +41,14 @@ const Home = () => {
             <h1>Email:</h1>
           </div>
           <div className='font-semibold space-y-2'>
-            <h1>Parashar</h1>
-            <h1>das</h1>
-            <h1>easf@asf.safd</h1>
+            <h1>{HomeUser.FirstName}</h1>
+            <h1>{HomeUser.LastName}</h1>
+            <h1>{HomeUser.Email}</h1>
           </div>
         </div>
         <button className='text-xl font-bold bg-green-500 w-full py-2 rounded-lg my-3' onClick={LogOutAll}>SignOut</button>
         <ToastContainer/>
-      </Container> 
+      </Container> : <p>Loading...</p>}
     </div>
   )
 }
